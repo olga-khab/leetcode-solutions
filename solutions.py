2e58877719e60a0b57fd642877d79e955e158572
olga-khab <olga.a.khab@gmail.com>
2021-02-21 18:01:54 +1100

    twoSum II sorted

--- solutions.py
+++ solutions.py
@@ -1,9 +1,22 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dct = {}
-        for i in range(0,len(nums)):
-            dct[nums[i]] = i
-        for i in range (0,len(nums)):
-            diff = target - nums[i]
-            if diff in dct and dct.get(diff) != i:
-                return [i,dct.get(diff)]
+    def twoSum(self, nums, target):
+        dict = {}
+        for i, n in enumerate(nums):
+            if target-n in dict:
+                return(i,dict[target-n])
+            else:
+                dict[n]=i
+                
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        diff_set = set()
+        return Solution.treeSearch(root,diff_set,k)
+
+    def treeSearch(node, diff_set,k):
+        current_set = diff_set
+        if node == None:
+            return False
+        elif node.val in current_set:
+            return True
+        else:
+            current_set.add(k-node.val)
+            return Solution.treeSearch(node.right, current_set,k) or Solution.treeSearch(node.left, current_set,k)
