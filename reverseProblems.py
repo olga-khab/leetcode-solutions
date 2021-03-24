0723cb854b9b4ede6cf02d5211262f55844fdd2d
olga-khab <olga.a.khab@gmail.com>
2021-03-24 11:34:50 +1100

    number of islands - medium

--- reverseProblems.py
+++ reverseProblems.py
@@ -1,5 +1,5 @@
 class Solution:
-  def reverse(self, x: int) -> int:
+    def reverse(self, x: int) -> int:
       flag = 1 if (x >= 0) else -1
       rev = ""
       x=abs(x)
@@ -11,3 +11,19 @@ class Solution:
           return 0
       else:
           return flag*int(rev)
+
+  # reverse a binary unsigned integer (return decimal)
+    def reverseBits(self, n: int) -> int:
+        num = bin(n)[2:]
+        num = "0"*(32-len(num))+num
+        num = num[::-1]
+        return int(num,2)
+ # counts number of 1s in a 32-bit binary integer
+    def hammingWeight(self, n: int) -> int:
+        bits = 0
+        mask = 1
+        for i in range(0,32):
+            if n & mask :
+                bits += 1
+            mask <<= 1
+        return bits
--- solutions.py
+++ solutions.py
@@ -20,3 +20,25 @@ class Solution:
         else:
             current_set.add(k-node.val)
             return Solution.treeSearch(node.right, current_set,k) or Solution.treeSearch(node.left, current_set,k)
+
+    # Number of Islands
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, r, c) -> List[List[str]]:
+            if (r >= len(grid) or c >= len(grid[0]) or r < 0 or c < 0 or grid[r][c]=="0"):
+                return grid
+            grid[r][c] = "0"
+            dfs(grid, r, c-1)
+            dfs(grid, r-1, c)
+            dfs(grid, r+1, c)
+            dfs(grid, r, c-1)
+            dfs(grid, r, c+1)
+        
+        count = 0
+        for i in range (0, len(grid)):
+            for j in range (0, len(grid[0])):
+                if (grid[i][j] == "1"):
+                    count+=1
+                    # trigger dfs - return new grid
+                    dfs(grid, i, j)
+        
+    return count
